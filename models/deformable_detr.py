81519b1e1ea9cffa50d307feffae616e64524b9b
Ekaterina Aidova <ekaterina.aidova@intel.com>
2021-02-02 15:26:55 +0300

    fix number of classes in model for vg training

--- models/deformable_detr.py
+++ models/deformable_detr.py
@@ -445,6 +445,8 @@ def build(args):
     num_classes = 20 if args.dataset_file != 'coco' else 91
     if args.dataset_file == "coco_panoptic":
         num_classes = 250
+    if agrs.dataset_file == "vg":
+        num_classes = 1601
     device = torch.device(args.device)
 
     backbone = build_backbone(args)
